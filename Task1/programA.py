29676a3ba600727ab76c2ef7e5b8a031aeb06c87
AbdiRaqiibMohummed <aclipz0121@gmail.com>
2026-03-09 12:33:08 +0000

    structuring the project

---
+++ Task1/programA.py
@@ -0,0 +1,9 @@
+# 1) Convert (decimal à hex and 16-bit binary)
+# 2) Little-endian pack/unpack (16-bit)
+# 3) ASCII memory dump
+# 4) Array addressing
+# 5) Stack frame (bp offsets)
+# 0) Exit
+# The user enters an option (0–5). The programme performs that option, then returns to the menu
+# until the user chooses 0.
+
